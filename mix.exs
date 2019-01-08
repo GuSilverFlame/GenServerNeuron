a6566819f7870ffb52ddcad3f56663e73a563940
Gustavo Santos Ferreira <gustavosantosfe@gmail.com>
2019-01-08 11:28:18 -0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Neuron.MixProject do
   def project do
     [
       app: :neuron,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
